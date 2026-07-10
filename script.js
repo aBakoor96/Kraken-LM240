4dd25e8b78c912016daceb6939daf7a3a246aa65
aBakoor96 <abakoor996@gmail.com>
2026-07-10 20:20:04 +0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("NZXT dashboard loaded");
+window.nzxt = {
+
+
+v1:{
+
+
+onMonitoringDataUpdate:function(data){
+
+
+let cpu="--";
+let gpu="--";
+
+
+
+if(data.cpus && data.cpus.length){
+
+cpu=Math.round(
+data.cpus[0].temperature
+);
+
+}
+
+
+
+if(data.gpus && data.gpus.length){
+
+gpu=Math.round(
+data.gpus[0].temperature
+);
+
+}
+
+
+
+document
+.getElementById("cpuTemp")
+.innerHTML=cpu;
+
+
+
+document
+.getElementById("gpuTemp")
+.innerHTML=gpu;
+
+
+
+}
+
+
+
+}
+
+
+
+};
